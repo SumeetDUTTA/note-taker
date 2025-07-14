e3163d7e2053d7a6d41fd73b206a0fafe6d1c211
Sumeet <sumeetdutta040@gmail.com>
2025-07-14 16:47:08 +0530

    feat: integrate note-taker package and configure API base URL for development and production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import notesRoutes from './routes/notesroutes.js';
 import connectDB from './config/db.js';
 import ratelimiter from './middleware/ratelimiter.js';
+import { env } from 'process';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve()
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-}))
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: 'http://localhost:5173',
+  }))
+}
+
 app.use(express.json());
 app.use(ratelimiter);
 
@@ -24,6 +30,13 @@ app.use(ratelimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if(process,env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"))
+  })
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+// In development, use the local server URL
+// In production, use the relative path to the API
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: BASE_URL,
 });
+
+
 
 export default api;
